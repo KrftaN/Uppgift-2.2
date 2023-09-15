bdaed7d603a6d395c49ba96a7aa3c220b27c6a7e
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-15 12:35:45 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elins hopp i meter?");
+            double elinLängd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Almas hopp i meter?");
+            double almaLängd = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(
+                "Elin hoppade " + (Math.Round(elinLängd - almaLängd, 3)) + " meter längre än Alma"
+            );
+            Console.ReadKey();
+        }
+    }
+}
